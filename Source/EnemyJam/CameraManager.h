78378a6122951821279217f3b8cfe19ee57f7775
AbertayFraxle <91606018+AbertayFraxle@users.noreply.github.com>
2023-09-25 21:54:35 +0100

    started cameramanger class

---
+++ Source/EnemyJam/CameraManager.h
@@ -0,0 +1,26 @@
+// Fraser McCann + Callum Shepherd AGDS FRESHERS JAM 2023
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CameraManager.generated.h"
+
+UCLASS()
+class ENEMYJAM_API ACameraManager : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACameraManager();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
